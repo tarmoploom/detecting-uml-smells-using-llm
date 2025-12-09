d233d73d5d294432987bdffd13a0255195f362fd
Tarmo Ploom <taploo@taltech.ee>
2025-12-09 02:49:11 +0200

    Results: Gemini Synthetic/Multi

--- save_result.py
+++ save_result.py
@@ -11,7 +11,7 @@ from datetime import datetime, timezone
 check_list = ["G5.1", "G5.2", "G6.1", "G6.2", "G6.3", "G8.1", "G15.1", "G15.2", "G15.3", "S1.1", "S1.2", "S2.1", "N1.1", "N1.2", "N3.1", "N3.2", "N3.3", "N4.1"]
 
 # Path Configuration
-end_path = "Synthetic/Single"
+end_path = "Synthetic/Multi"
 llm_model_name = "Gemini 3 Pro"
 
 # Calculate absolute paths based on script location
